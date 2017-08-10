d8f51b61dc2d41c45b92329dea5bfa32942ccb76
Josip Brčina <info1x2@gmail.com>
2017-08-10 15:05:32 +0200

    Finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '571353206860-mm3731tqihg323erut9n5q9cbjm7sata.apps.googleusercontent.com',
+  googleClientSecret: 'NUvGFh6OVGqFlBdBcFq9lUdQ',
+  mongoURI: 'mongodb://dbuser:password@ds129532.mlab.com:29532/emaily-dev',
+  cookieKey: '353j5k35jkh353583583953535jnk33683448358ksfjsjfhsfkjhskjfjksfhs'
+};
---
+++ services/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+// Create token for cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Retrieve user model from cookie
+passport.deserializeUser((userId, done) => {
+  User.findById(userId)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    // Executed after the callback (google authentication)
+    (accessToken, refreshToken, profile, done) => {
+      // Find user
+      User.findOne({ googleId: profile.id})
+        .then(existingUser => {
+          if (existingUser) {
+            // User exists, call done
+            done(null, existingUser);
+          } else {
+            // User doesn't exist, create new user inside MongoDb
+            new User({ googleId: profile.id })
+              .save()
+              .then(savedUser => done(null, savedUser));
+          }
+      })
+    }
+  )
+);
